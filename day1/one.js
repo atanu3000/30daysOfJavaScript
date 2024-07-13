cf86d4f2e4efa42286bd20128236bfa5736686dc
Atanu Paul <atanupaul2411@gmail.com>
2024-07-13 23:29:12 +0530

    day 1 complete

---
+++ day1/one.js
@@ -0,0 +1,30 @@
+// Variable Assignment
+var num = 9;
+console.log(num);
+
+// let Assignment
+let text = "Hello World";
+console.log(text);
+
+// const Assignment
+const check = true;
+console.log(check);
+
+// Data types and use of type of 
+const string = "Hello World";
+const number = 55;
+const boolean = false;
+const object = { name: "Atanu" };
+const arr = [1, 2, "chai"];
+console.log(typeof string);
+console.log(typeof number);
+console.log(typeof boolean);
+console.log(typeof object);
+console.log(Array.isArray(arr)); // array is a special type of object 
+
+// reassignment of value to a variable
+text = "Hello World Again";
+console.log(text);
+
+// const variable cannot be changed after assignment
+// check = true;  
